ccf271081aa7ee0b05f2461115ffd0cc46bb7390
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-01 12:53:12 +0530

    Added post feedback api

--- public/assets/templates/feedbacks.js
+++ public/assets/templates/feedbacks.js
@@ -15,3 +15,47 @@ stars.forEach((star, index1) => {
     });
   });
 });
+
+
+const feedbackSubmit = document.getElementById("feedback-submit");
+
+feedbackSubmit.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const name = document.getElementById("feedback-user").value;
+  const email = document.getElementById("feedback-email").value;
+  const phone = document.getElementById("feedback-phone").value;
+  const designation = document.getElementById("feedback-designation").value;
+  const feedback = document.getElementById("feedback-message").value;
+  const rating = document.querySelectorAll("#stars i.fa-star").length;
+
+  const data = {
+    "designation": designation,
+    "email": email,
+    "feedback": feedback,
+    "name": name,
+    "phone": phone
+  }
+
+  // post api using fetch
+
+  const response = await fetch("https://nslondon.onrender.com/feedback/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  console.log("response : ",response);
+  
+});
+
+const fetchFeedbacks = async () => {
+  const response  = await fetch("https://nslondon.onrender.com/feedback/");
+  const data = await response.json();
+
+  console.log(data);
+};
+
+
+fetchFeedbacks();
