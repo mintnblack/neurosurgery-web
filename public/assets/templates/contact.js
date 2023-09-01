6e6af1910ff9983eb47fe55a387b72e579b707cf
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-01 13:14:21 +0530

    Integrated post contact info api

---
+++ public/assets/templates/contact.js
@@ -0,0 +1,28 @@
+
+
+const contactFormSubmitBtn = document.getElementById("contactFormSubmitBtn");
+
+contactFormSubmitBtn.addEventListener("click", async()=>{
+    const name = document.getElementById("contact-name").value;
+    const phone = document.getElementById("contact-Phone").value;
+    const email = document.getElementById("contact-email").value;
+    const message = document.getElementById("contact-message").value;
+
+    const data = {
+        name,
+        phone,
+        email,
+        message
+    }
+
+    const response = await fetch("https://nslondon.onrender.com/contact/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  console.log(response)
+
+});
