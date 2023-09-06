81dec4888a3e82204823fe471276578ce4c6e580
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-06 15:03:00 +0530

    Fixed appoinment form

--- public/assets/templates/appointment.js
+++ public/assets/templates/appointment.js
@@ -3,7 +3,7 @@ import { BASE_URL } from "../utils/applicationConstants.js";
 const clinicNames = document.getElementById("appointment_dropdown_content");
 
 window.onSelectClinicName = async (index) => {
-  const response = await fetch(`${BASE_URL}/clinic/`);
+  const response = await fetch(`${BASE_URL}/clinic/20/1`);
   const data = await response.json();
   document.getElementById("appointment_clinic").innerHTML =
     data.data[index].name;
@@ -11,7 +11,7 @@ window.onSelectClinicName = async (index) => {
 };
 
 const fetchAllClinicData = async () => {
-  const response = await fetch(`${BASE_URL}/clinic/`);
+  const response = await fetch(`${BASE_URL}/clinic/20/1`);
   const data = await response.json();
 
   const clinicData = data.data;
@@ -39,15 +39,30 @@ bookAppointmentBtn.addEventListener("click", async(e) => {
   const age = document.getElementById("appointment_age").value;
   const date = document.getElementById("appointment_date").value;
   const gender = document.getElementById("appointment_gender").value;
+  const authorisation = document.getElementById("appointment_authorisation").value;
+  const insuranceRadioGroup = document.getElementsByName("appointment-insurace");
+  let insurace;
+
+  for (const radioButton of insuranceRadioGroup) {
+    if (radioButton.checked) {
+      insurace = radioButton.value;
+      break; // Exit the loop once a selected radio button is found
+    }
+  }
+
+  console.log(insurace, authorisation)
 
   const data = {
     age: age,
     clinic: appointmentClinicName,
+    clinic_id: appointmentClinicId,
     date: date,
     email: email,
     gender: gender,
     name: username,
-    phone: phone
+    phone: phone,
+    insurance: insurace,
+    authorisation: authorisation
   };
 
   const response = await fetch(`${BASE_URL}/appointment/`, {
