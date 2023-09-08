11d59bdfc1080d665027fbf55cbaac0ca17a7253
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-08 13:41:03 +0530

    Fixed display feedback section

--- public/assets/templates/displayFeedback.js
+++ public/assets/templates/displayFeedback.js
@@ -4,25 +4,25 @@ const userName = document.querySelectorAll(".testimonial__meta-title");
 const userJob = document.querySelectorAll(".testimonial__meta-desc");
 const userComments = document.querySelectorAll(".testimonial__title");
 
+console.log(userName);
+console.log(userJob);
+console.log(userComments);
+
 const fetchFeedbacks = async () => {
   const response = await fetch(`${BASE_URL}/feedback/approved/20/1`);
   const data = await response.json();
   const feedbacks = data.data;
 
-  feedbacks.map((feedback, index) => {
-    userName[index].innerHTML = feedback.name;
-    userJob[index].innerHTML = feedback.designation;
-    userComments[index].innerHTML = feedback.feedback;
-  });
-
-  if (feedbacks.length < 3) {
-    const length = 3 - feedbacks.length;
-    console.log(length);
-    for (let i = 0; i < length; i++) {
-      userName[feedbacks.length + i].innerHTML = "Anonymous";
-      userJob[feedbacks.length + i].innerHTML = "Anonymous";
-      userComments[feedbacks.length + i].innerHTML = "Anonymous";
-    }
+  if (feedbacks.length >= 3) {
+    feedbacks.map((feedback, index) => {
+      userName[index].innerHTML = feedback.name;
+      userJob[index].innerHTML = feedback.designation;
+      userComments[index].innerHTML = feedback.feedback;
+    });
+  } else {
+    const testimonials = document.querySelectorAll(".testimonials-layout1");
+    console.log("testimonials", testimonials);
+    testimonials[0].style.display = "none";
   }
 };
 
