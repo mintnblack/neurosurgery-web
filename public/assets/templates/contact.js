7dede78dbdae0329a01c01bf9c292d0a9e7946d5
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-15 21:31:02 +0530

    Added form validation in contact form

--- public/assets/templates/contact.js
+++ public/assets/templates/contact.js
@@ -1,29 +1,69 @@
-
 import { BASE_URL } from "../utils/applicationConstants.js";
+import { setAlertAction } from "../functions/setAlertAction.js";
+
+const name = document.getElementById("contact-name");
+const phone = document.getElementById("contact-Phone");
+const email = document.getElementById("contact-email");
+const message = document.getElementById("contact-message");
 
 const contactFormSubmitBtn = document.getElementById("contactFormSubmitBtn");
 
-contactFormSubmitBtn.addEventListener("click", async()=>{
-    const name = document.getElementById("contact-name").value;
-    const phone = document.getElementById("contact-Phone").value;
-    const email = document.getElementById("contact-email").value;
-    const message = document.getElementById("contact-message").value;
+const handleValidation = () => {
+  let isValid = true;
+  if (name.value === "") {
+    isValid = false;
+    setAlertAction("Please enter your name", "danger");
+  }else if(phone.value === ""){
+    isValid = false;
+    setAlertAction("Please enter your phone number", "danger");
+  }else if(phone.value.length !== 10){
+    isValid = false;
+    setAlertAction("Please enter valid phone number", "danger");
+  }else if(phone.value.includes(" ")){
+    isValid = false;
+    setAlertAction("Please enter valid phone number", "danger");
+  }else if(email.value === ""){
+    isValid = false;
+    setAlertAction("Please enter your email", "danger");
+  }else if(!email.value.includes("@")){
+    isValid = false;
+    setAlertAction("Please enter valid email", "danger");
+  }else if(!email.value.includes(".")){
+    isValid = false;
+    setAlertAction("Please enter valid email", "danger");
+  }else if(email.value.includes(" ")){
+    isValid = false;
+    setAlertAction("Please enter valid email", "danger");
+  }else if(email.value.includes("@.")){
+    isValid = false;
+    setAlertAction("Please enter valid email", "danger");
+  }else if(message.value === ""){
+    isValid = false;
+    setAlertAction("Please enter your message", "danger");
+  }
+  return isValid;
+};
 
+contactFormSubmitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (handleValidation()) {
     const data = {
-        name,
-        phone,
-        email,
-        message
-    }
+      name: name.value,
+      phone: phone.value,
+      email: email.value,
+      message: message.value,
+    };
 
-    const response = await fetch(`${BASE_URL}/contact/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+    console.log(data);
 
-  console.log(response)
+    //   const response = await fetch(`${BASE_URL}/contact/`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // });
 
+    // console.log(response)
+  }
 });
