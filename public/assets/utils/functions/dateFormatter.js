7b105ba3c84e2cf6cde02826826264c540d795e0
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-20 13:59:35 +0530

    Fixed list all blogs api in website

---
+++ public/assets/utils/functions/dateFormatter.js
@@ -0,0 +1,19 @@
+const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November","December"];
+const monthArrayShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov","Dec"];
+
+export function formatDateToDDMMYY(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Note: Months are zero-based, so we add 1.
+    const year = String(date.getFullYear()).slice(-2); // Extract last two digits of the year.
+    
+    const formattedDate = {
+        day: day,   
+        month: month,
+        year: year,
+        monthName: monthArray[month-1],
+        monthNameShort: monthArrayShort[month-1]
+    }
+
+    return formattedDate;
+  }
