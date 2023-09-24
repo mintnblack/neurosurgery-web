58f6ac25a68414c17a722acc5a5d9c78a3184ae5
Sangeetha SR <sangeetharajan567@gmail.com>
2023-09-24 20:50:11 +0530

    Added data into single blog page

--- public/assets/templates/singleBlog.js
+++ public/assets/templates/singleBlog.js
@@ -1,19 +1,27 @@
 
 import { BASE_URL } from "../utils/applicationConstants.js";
+import { formatDateToDDMMYY } from "../utils/functions/dateFormatter.js"
 
 const params = new URLSearchParams(window.location.search);
 const blogId = params.get("blogId");
-
-console.log(blogId)
 
 const fetchSingleBlog = async () => {
     const response = await fetch(`${BASE_URL}/blog/${blogId}`);
     const data = await response.json();
     const singleBlogData = data.data;
     console.log(singleBlogData)
-
+    const date = formatDateToDDMMYY(singleBlogData.updated);
+    console.log(date)
+    document.getElementById('postDate').innerHTML = `${date.monthNameShort} ${date.day}, ${date.year}`;
     document.getElementById('postDescription').innerHTML = singleBlogData.html;
-
+    document.getElementById('postAuthor').innerHTML = singleBlogData.author;
+    document.getElementById('postTitle').innerHTML = singleBlogData.title;
+    const postTags = document.getElementById('postTags');
+    let tagInnerHtml = '';
+    singleBlogData.tags.forEach(tag => {
+        tagInnerHtml += `<li><a href="#">${tag}</a></li>`
+    });
+    postTags.innerHTML = tagInnerHtml;
 };
 
 fetchSingleBlog();
